4e23535f899df224a1508fb9999b606862d1e5b3
Everton Brzozowy Alves <everton.brzozowy.alves@gmail.com>
2021-07-15 22:27:24 -0300

    isolated server

--- bin/server.js
+++ bin/server.js
@@ -1,28 +1,16 @@
-'use strict' //forces the javascript to follow a standard
-
-const http = require('http');
+const app = require('../src/app');
 const debug = require('debug')('nodestr:server');
-const express = require('express');
+const http = require('http');
 
-const app = express();
+// const app = express();
 const port = normalizePort(process.env.PORT || '3000'); //if we do not have a port on environment, use the 3000
 app.set('port', port);
 
 const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    })
-});
-
-app.use('/', route);
 
 server.listen(port);
 server.on('error', onError);
-
+server.on('listening', onListening);
 console.log('API started at port ' + port);
 
 function normalizePort(val) {
@@ -56,4 +44,12 @@ function onError(error) {
         default:
             throw error;
     }
+}
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
 }
